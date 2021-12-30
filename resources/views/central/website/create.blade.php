86a662b5c61bbece7bdb4ad58c290737bdcebf19
imoveis <contato@wc4.com.br>
2021-12-30 18:02:31 -0300

    Nova correção de cabecalho da tela de login

--- resources/views/central/website/create.blade.php
+++ resources/views/central/website/create.blade.php
@@ -4,9 +4,10 @@
       <!-- Required meta tags -->
       <meta charset="utf-8">
       <meta name="csrf-token" content="{{ csrf_token() }}">
-      <!-- Bootstrap CSS -->
-      <link rel="stylesheet" href="https://unpkg.com/@tabler/core@latest/dist/css/tabler.min.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+      <meta http-equiv="X-UA-Compatible" content="ie=edge"/>  
       <title>{{ config('app.name', 'Laravel') }}</title>
+      <link rel="stylesheet" href="https://unpkg.com/@tabler/core@latest/dist/css/tabler.min.css">
    </head>
    <body>
       <div class="page page-center">
